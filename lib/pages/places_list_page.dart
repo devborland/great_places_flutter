7cb64f957ad4476f2c2ad98092b11a85afcde856
Dev Borland <devborland@gmail.com>
2021-03-11 16:35:35 +0300

    Storing and Fetching Data with Sqflite

--- lib/pages/places_list_page.dart
+++ lib/pages/places_list_page.dart
@@ -21,23 +21,33 @@ class PlacesListPage extends StatelessWidget {
           ),
         ],
       ),
-      body: Consumer<GreatPlaces>(
-        child: const Center(
-          child: const Text('Got no places yet, start adding some!'),
-        ),
-        builder: (ctx, greatPlaces, ch) => greatPlaces.items.length <= 0
-            ? ch
-            : ListView.builder(
-                itemCount: greatPlaces.items.length,
-                itemBuilder: (ctx, i) => ListTile(
-                  leading: CircleAvatar(
-                    backgroundImage: FileImage(greatPlaces.items[i].image),
-                  ),
-                  title: Text(greatPlaces.items[i].title),
-                  onTap: () {
-                    //... Go to detail page
-                  },
+      body: FutureBuilder(
+        future: Provider.of<GreatPlaces>(
+          context,
+          listen: false,
+        ).fetchPlaces(),
+        builder: (ctx, snapshot) => snapshot.connectionState ==
+                ConnectionState.waiting
+            ? Center(child: CircularProgressIndicator())
+            : Consumer<GreatPlaces>(
+                child: const Center(
+                  child: const Text('Got no places yet, start adding some!'),
                 ),
+                builder: (ctx, greatPlaces, ch) => greatPlaces.items.length <= 0
+                    ? ch
+                    : ListView.builder(
+                        itemCount: greatPlaces.items.length,
+                        itemBuilder: (ctx, i) => ListTile(
+                          leading: CircleAvatar(
+                            backgroundImage:
+                                FileImage(greatPlaces.items[i].image),
+                          ),
+                          title: Text(greatPlaces.items[i].title),
+                          onTap: () {
+                            //... Go to detail page
+                          },
+                        ),
+                      ),
               ),
       ),
     );
